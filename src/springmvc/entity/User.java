ee04d737f939c542842b8a8a806069bfc6ca1e98
SeasonTemple <512743454@qq.com>
2019-05-02 11:54:36 +0800

    register validator

--- src/springmvc/entity/User.java
+++ src/springmvc/entity/User.java
@@ -1,5 +1,6 @@
 package springmvc.entity;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
@@ -7,19 +8,19 @@ public class User {
 	
 	private int uid;
 	
-	@Size
-	@NotBlank
+	@Size(min = 4, max = 20, message = "用户名不在4-20字符之内!")
+	@NotBlank(message = "用户名不能为空!")
 	private String uname;
 	
-	@Size
-	@NotBlank
+	@Size(min = 4, max = 20, message = "密码应在4-20字符之内!")
+	@NotBlank(message = "密码不能为空!")
 	private String pwd;
 	
-	@NotBlank
+	@NotBlank(message = "邮箱地址不能为空!")
 	private String email;
 	
-	@Size
-	@NotBlank
+	@Min(value = 2 , message = "职称名应不小于2个字符!")
+	@NotBlank(message = "职称名不能为空!")
 	private String profile;
 	private String sex;
 	private String code;
