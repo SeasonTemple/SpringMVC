6266fa2c1b0c77911660fb0f3b0440d79f6dd5ae
SeasonTemple <512743454@qq.com>
2019-06-16 18:04:09 +0800

    ProviderHelper

--- src/springmvc/dao/provider/StudentDynaSqlProvider.java
+++ src/springmvc/dao/provider/StudentDynaSqlProvider.java
@@ -7,31 +7,45 @@ import springmvc.entity.Student;
 import static springmvc.util.SpringMvcConstants.STUDENTTABLE;;
 
 public class StudentDynaSqlProvider {
+	
+	
+	//动态更新学生信息
+	public String updateStudent(Student s) {
 
-public String updateStudent(Student s){
-		
-		return new SQL(){
+		return new SQL() {
 			{
 				UPDATE(STUDENTTABLE);
-				if(s.getSname()!= null){
+				if (s.getSname() != null) {
 					SET(" sname = #{sname} ");
 				}
-				if(s.getSnum()!= 0){
+				if (s.getSnum() != 0) {
 					SET(" snum = #{snum} ");
 				}
-				if(s.getGrade()!= 0){
+				if (s.getGrade() != 0) {
 					SET(" grade = #{grade} ");
 				}
-				if(s.getSubject()!= null){
+				if (s.getSubject() != null) {
 					SET(" subject = #{subject} ");
 				}
-				if(s.getClas()!= null){
+				if (s.getClas() != null) {
 					SET(" cid = #{clas.cid} ");
 				}
-				if(s.getU() != null){
+				if (s.getU() != null) {
 					SET(" uid = #{u.uid} ");
 				}
 				WHERE(" sid = #{sid} ");
+			}
+		}.toString();
+	}
+	
+	public String searchStudent(Student s) {
+		return new SQL() {
+			{
+				SELECT("*");
+				FROM(STUDENTTABLE);
+				if(s!=null) {
+					
+				}
 			}
 		}.toString();
 	}
---
+++ src/springmvc/util/ProviderHelper.java
@@ -0,0 +1,12 @@
+package springmvc.util;
+/**
+ * 该方法为Provider提供解析后的参数，以用于provider的模糊查询
+ * @author Season-Temple
+ * 2019年6月16日 下午6:03:21
+ * package springmvc.util
+ * class-name ProviderHelper.java
+ */
+public class ProviderHelper {
+	
+	
+}
